20ac79c0aabe36cc39d60667b7bfec8aa7979091
MMontoya5 <mateo.montoya@patelhs.org>
2020-10-08 14:42:16 +0000

    I added my if else statement if(!morning)     {       System.out.print("I need to ");     }     else     {       System.out.print("To early to ");     }     System.out.println(" wake up" );   } }

--- Main.java
+++ Main.java
@@ -1,12 +1,16 @@
 class Main 
 {
-  public static void main(String[] args)
-   {
-     boolean morning = false;
-     if (!morning);  
+  public static void main(String[] args) 
+  {
+    boolean morning = false; 
+    if(!morning)
     {
-         
+      System.out.print("I need to "); 
     }
-   
-   }  
+    else
+    {
+      System.out.print("To early to "); 
+    }
+    System.out.println(" wake up" );
+  }
 }
