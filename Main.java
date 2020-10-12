cff6abf2ec32cecb20961ff035ef4052fd1bed4b
MMontoya5 <mateo.montoya@patelhs.org>
2020-10-12 12:41:05 +0000

    Finished my for loop

--- Main.java
+++ Main.java
@@ -13,6 +13,15 @@ class Main
     {
       System.out.print("To early to "); 
     }
-    System.out.println("wake up" );
-  }
+      System.out.println("wake up and workout.");
+      System.out.println("I need to do 10 pushups."); 
+  
+  for (int count = 1; count <= 10;count++)
+    {
+     System.out.println(count + " pushup "); 
+    }
+    {
+     System.out.println("Am done, now to situps.");
+    }
+  }  
 }
